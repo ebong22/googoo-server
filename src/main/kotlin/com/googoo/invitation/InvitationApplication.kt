6974636174f00ca1c8a2179e9085613a93c6256c
ebong <lhlbl@naver.com>
2023-01-17 18:58:19 +0900

    Initial commit

---
+++ src/main/kotlin/com/googoo/invitation/InvitationApplication.kt
@@ -0,0 +1,11 @@
+package com.googoo.invitation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvitationApplication
+
+fun main(args: Array<String>) {
+	runApplication<InvitationApplication>(*args)
+}
