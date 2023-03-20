77fc8810c395db0de23e82f4b89127f5447af161
ebong <lhlbl@naver.com>
2023-03-21 06:50:02 +0900

    git username test commit
    Signed-off-by: ebong <lhlbl@naver.com>

--- src/main/kotlin/com/googoo/invitation/domain/Message.kt
+++ src/main/kotlin/com/googoo/invitation/domain/Message.kt
@@ -11,7 +11,7 @@ class Message (
     val name: String,
     val password: String,
     val contents: String,
-    var isDelete: Boolean = false // 삭제여부 (추후 복구 요청이 있을 수도 있기 때문에)
+    var isDelete: Boolean = false // 삭제여부 (추후 복구 요청이 있을 수도 있기 때문에 사용)
 ) {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "invitation_id")
